b0af8e3c8f69abb671d329e49928e92eacedf4eb
Flor <florcardona91@gmail.com>
2017-04-07 07:18:00 -0500

    Creación del proyecto

---
+++ FormSeleccionPersonal/FormSeleccionPersonal/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FormSeleccionPersonal.Startup))]
+namespace FormSeleccionPersonal
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
