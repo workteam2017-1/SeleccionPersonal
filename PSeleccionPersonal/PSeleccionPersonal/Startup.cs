ae4195712eaafea051594b7c7745a62c298da58a
Flor <florcardona91@gmail.com>
2017-04-07 07:23:56 -0500

    Creación de Proyecto

---
+++ PSeleccionPersonal/PSeleccionPersonal/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PSeleccionPersonal.Startup))]
+namespace PSeleccionPersonal
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
